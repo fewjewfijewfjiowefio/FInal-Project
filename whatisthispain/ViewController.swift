97c092a8b95e1dbbf4a2b3940cf9d411aff05541
juwayriya khan <juwayriyakhan@lhswildcats.org>
2021-12-20 09:03:56 -0600

    Initial Commit

---
+++ whatisthispain/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  whatisthispain
+//
+//  Created by Juwayriya Khan on 11/29/21.
+//
+
+import UIKit
+
+// Location
+// table view
+// custom cell: collection view
+// API / request to get the data
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
